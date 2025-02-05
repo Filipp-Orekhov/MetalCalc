9ec6dc8944658b7db9815a23915f30090da6828c
Filipp-Orekhov <filipp.orekhov85@gmail.com>
2025-02-05 14:41:32 +0300

    project initiation

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -13,24 +13,18 @@ interface CacheEntry<T> {
 const memoryCache: Record<string, CacheEntry<unknown>> = {};
 const TTL = 3600000;
 
-// Запрос: проверяем кеш перед отправкой на сервер
 axiosInstance.interceptors.request.use((config) => {
   if (config.method === "get" && config.url) {
-    // 1️⃣ Проверяем кеш в памяти
     const cachedMemory = memoryCache[config.url];
     if (cachedMemory && Date.now() - cachedMemory.timestamp < TTL) {
-      console.log(`✅ Используем кеш (RAM) для ${config.url}`);
       return Promise.reject({ data: cachedMemory.data, fromCache: true });
     }
 
-    // 2️⃣ Проверяем кеш в `localStorage`
     const cachedLocal = localStorage.getItem(config.url);
     if (cachedLocal) {
       try {
         const { data, timestamp } = JSON.parse(cachedLocal);
         if (Date.now() - timestamp < TTL) {
-          console.log(`✅ Используем кеш (localStorage) для ${config.url}`);
-          // Сохраняем в память (RAM) для быстрого доступа
           memoryCache[config.url] = { data, timestamp };
           return Promise.reject({ data, fromCache: true });
         }
@@ -42,19 +36,15 @@ axiosInstance.interceptors.request.use((config) => {
   return config;
 });
 
-// Ответ: сохраняем в кеш
 axiosInstance.interceptors.response.use(
   (response) => {
     if (response.config.method === "get" && response.config.url) {
-      console.log(`💾 Кешируем данные для ${response.config.url}`);
 
-      // 1️⃣ Кешируем в память (RAM)
       memoryCache[response.config.url] = {
         data: response.data,
         timestamp: Date.now(),
       };
 
-      // 2️⃣ Кешируем в `localStorage`
       localStorage.setItem(
         response.config.url,
         JSON.stringify({ data: response.data, timestamp: Date.now() })
@@ -63,7 +53,6 @@ axiosInstance.interceptors.response.use(
     return response;
   },
   (error) => {
-    // Если данные есть в кеше, возвращаем их
     if (error.fromCache) {
       return Promise.resolve({ data: error.data });
     }
