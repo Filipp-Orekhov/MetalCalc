ae161358abbf3220c0e64a82f033766ce7f2cbfe
Filipp-Orekhov <filipp.orekhov85@gmail.com>
2025-06-25 22:08:59 +0300

    vite-config-fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['metalcalc-react.onrender.com'],
+  },
   base: '/MetalCalc/',
+  server: {
+    allowedHosts: ['metalcalc-react.onrender.com'],
+  },
 });
